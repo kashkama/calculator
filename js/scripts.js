50253367f17a38bdecb88b3afbd6564202a1b28a
kashkama <owen.kamau2014@gmail.com>
2016-11-23 08:23:59 +0300

    adding html file with a script link

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var operations = function(num1,num2){
+	return num1 + num2;
+};
+var num1 = parseInt(prompt("enter a number:"));
+var num2 = parseInt(prompt("enter another number:"));
+var result = operations(num1,num2);
+alert(result);
